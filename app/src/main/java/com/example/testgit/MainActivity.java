06ac82c7055b1bd6449459a4d5449fe1ebe159e3
myiswangfei <cjsswf@gmail.com>
2019-03-11 20:06:46 +0800

    我自己随便修改了一个MainActivity

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        lsadflksaf卡健身房的
+//        撒可怜见反对你猜伺服电机
+//        撒来看回复的卢卡斯发生了科技发达
+
+
+
+
     }
 }
